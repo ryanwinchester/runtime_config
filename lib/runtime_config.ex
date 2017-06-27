6e32652df450c6dea0f0333b606e6473bdd1bd4e
Ryan Winchester <code@ryanwinchester.ca>
2017-06-27 15:22:03 -0700

    Initial commit

---
+++ lib/runtime_config.ex
@@ -0,0 +1,18 @@
+defmodule RuntimeConfig do
+  @moduledoc """
+  Documentation for RuntimeConfig.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RuntimeConfig.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/runtime_config_test.exs
@@ -0,0 +1,8 @@
+defmodule RuntimeConfigTest do
+  use ExUnit.Case
+  doctest RuntimeConfig
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
